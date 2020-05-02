9be2df1457093f677850d3526ebf007d4b63b2a5
kik <henriquezanferrari@gmail.com>
2020-05-02 15:41:46 -0300

    Ordenado Crescente

---
+++ Algoritmos de Ordenacao/H - OrdenadoCrescente/OrdenadoCrescente.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char isOrdered(int array[], int size);
+
+int main()
+{
+    int size, i;
+    int array[10000];
+
+    while (scanf("\n%d", &size) != EOF)
+    {
+        for (i = 0; i < size; i++)
+        {
+            scanf(" %d", &array[i]);
+        }
+
+        printf("%c\n", isOrdered(array, size));
+    }
+}
+
+char isOrdered(int array[], int size)
+{
+    char ordered = 'S';
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        if (array[i + 1] < array[i])
+            ordered = 'N';
+    }
+
+    return ordered;
+}
